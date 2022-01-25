d7704783045967bc9090fd3d0ff44c5b5ec92287
Adnan BAL <adnbal89@hotmail.com>
2022-01-25 20:12:07 +0300

    Part 3 End - Navigation graph defined. - FragmentContainerView defined -> NavHostFragment

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/MainActivity.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.codinginflow.mvvmtodo
+package com.codinginflow.mvvmtodo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.codinginflow.mvvmtodo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
