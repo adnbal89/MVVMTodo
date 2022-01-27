27f0222f20c2b57a7bb0daa921208c9c3fe9407c
adnbal89 <adnbal89@gmail.com>
2022-01-27 16:43:40 +0300

    Part 7 - Combining multiple Flows - MVVM To-Do List App with Flow and Architecture Components
    -Option selections implemented.

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
 //constructor --> ()
@@ -21,10 +21,21 @@ class TasksViewModel @ViewModelInject constructor(
     //like a mutable live data
     val searchQuery = MutableStateFlow("")
 
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+
     //Whenever searchQuery changes, execute this block //"it" is value of current query.
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    private val tasksFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        taskDao.getTasks(query, sortOrder, hideCompleted)
     }
 
     val tasks = tasksFlow.asLiveData()
 }
+
+enum class SortOrder { BY_NAME, BY_DATE }
