3138259a0f622f64cf25ab9aa7c1b6f7e1fdf887
Adnan BAL <adnbal89@hotmail.com>
2022-01-30 13:17:31 +0300

    Part 10 - Swipe to Delete & Undo Tasks - Channel is implemented between viewModel and the fragment for decoupling reasons. - swipe implemented via onSwiped // ItemTouchHelper - UNDO implemented as insert fucntion.

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -8,9 +8,11 @@ import com.codinginflow.mvvmtodo.data.PreferencesManager
 import com.codinginflow.mvvmtodo.data.SortOrder
 import com.codinginflow.mvvmtodo.data.Task
 import com.codinginflow.mvvmtodo.data.TaskDao
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 //constructor --> ()
@@ -29,6 +31,13 @@ class TasksViewModel @ViewModelInject constructor(
 
     val preferencesFlow = preferencesManager.preferencesFlow
 
+    //channel instantiation. carries TasksEvent
+    //if we expose a channel to the outside, then consumer fragment can put something in it. we do not want that.
+    //to not to expose channel outside, we create tasksEvent val.
+    private val tasksEventChannel = Channel<TasksEvent>()
+
+    //basically turns this channel into a flow that we can than the fragment can use the single values out of it.
+    val tasksEvent = tasksEventChannel.receiveAsFlow()
 
     //Whenever searchQuery changes, execute this block //"it" is value of current query.
     //Using combine (inside FLow) to combine options above and apply for the data.
@@ -61,6 +70,29 @@ class TasksViewModel @ViewModelInject constructor(
     //because update is a suspend function, so we need a coroutine
     fun onTaskCheckedChanged(task: Task, isChecked: Boolean) = viewModelScope.launch {
         taskDao.update(task.copy(completed = isChecked))
+    }
+
+    //actual delete functionality implemented here.
+    //delete is a suspend funct. so we need a coroutine
+    //viewModel is responsible for showing the snackBar (undo method),
+    //only viewmodel knows when to show the snackbar but only the fragment or activity (context)
+    //can show a snackbar, so this functionality is a bit tricky, beware of that.
+    //viewModel never should contain any reference to a fragment or an activity, because of memory leak.
+
+    fun onTaskSwiped(task: Task) = viewModelScope.launch {
+        taskDao.delete(task)
+        //we use kotlin channels, we can send data between two kotlin coroutines
+        //channel instantiated above this class.
+        tasksEventChannel.send(TasksEvent.ShowUndoDeleteTaskMessage(task))
+    }
+
+    fun onUndoDeleteClick(task: Task) = viewModelScope.launch {
+        taskDao.insert(task)
+    }
+
+    //to represent different kinds of events to be able to send to the fragment.
+    sealed class TasksEvent {
+        data class ShowUndoDeleteTaskMessage(val task: Task) : TasksEvent()
     }
 }
 
